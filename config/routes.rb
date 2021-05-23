81cb5e5c2a9d6e766ae8d60e71c6dbbc7225baf5
Heather Mathies <hlmathies@gmail.com>
2021-05-22 21:10:38 -0400

    fixing routing to use home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :courses
-  root "static_pages#landing_page"
+  # root "static_pages#landing_page"
   # get 'static_pages/privacy_policy'
   # get 'static_pages/landing_page'
   get "privacy_policy", to: "static_pages#privacy_policy"
+  get 'home/index'
+  root 'home#index'
 end
+
